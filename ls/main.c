29f5d0c4d6526a5f02b9ca69656e5f3e7d35defe
Lance Hart <shipsimfan@hotmail.com>
2021-03-18 23:06:23 -0400

    Added file flags

--- ls/main.c
+++ ls/main.c
@@ -1,6 +1,30 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+
+int longList = 0;
+int showAll = 0;
+
+void ParseFlags(const char* ptr) {
+    while (*ptr) {
+        switch (*ptr) {
+        case 'l':
+            longList = 1;
+            break;
+
+        case 'a':
+            showAll = 1;
+            break;
+
+        default:
+            fprintf(stderr, "Unknown flag %c\n", *ptr);
+            exit(1);
+        }
+
+        ptr++;
+    }
+}
 
 int main(int argc, char* argv[]) {
     if (argc > 3) {
@@ -8,37 +32,19 @@ int main(int argc, char* argv[]) {
         return 1;
     }
 
-    int longList = 0;
     int locationArg = -1;
 
     if (argc == 2) {
-        if (argv[1][0] == '-') {
-            if (argv[1][1] == 'l')
-                longList = 1;
-            else {
-                fprintf(stderr, "Unknown flags %s\n", argv[1]);
-                return 1;
-            }
-        } else
+        if (argv[1][0] == '-')
+            ParseFlags(argv[1] + 1);
+        else
             locationArg = 1;
     } else if (argc == 3) {
         if (argv[1][0] == '-') {
-            if (argv[1][1] == 'l')
-                longList = 1;
-            else {
-                fprintf(stderr, "Unknown flags %s\n", argv[1]);
-                return 1;
-            }
-
+            ParseFlags(argv[1] + 1);
             locationArg = 2;
         } else if (argv[2][0] == '-') {
-            if (argv[2][1] == 'l')
-                longList = 1;
-            else {
-                fprintf(stderr, "Unknown flags %s\n", argv[1]);
-                return 1;
-            }
-
+            ParseFlags(argv[1] + 1);
             locationArg = 1;
         } else {
             fprintf(stderr, "Too many arguments!\n");
@@ -56,15 +62,15 @@ int main(int argc, char* argv[]) {
     unsigned long long num;
     dirent_t* entries = getdirent(&num);
 
-    if (longList) {
-        for (unsigned long long i = 0; i < num; i++) {
+    for (unsigned long long i = 0; i < num; i++) {
+        if (!showAll && (entries[i].flags & DIRENT_FLAG_HIDDEN))
+            continue;
+
+        if (longList) {
             printf("\t%s", entries[i].name);
             if (entries[i].type == DIRENT_TYPE_FILE)
                 printf(" (%lli KB)", entries[i].size / 1024);
-            printf("\n");
-        }
-    } else {
-        for (unsigned long long i = 0; i < num; i++)
+        } else
             printf("%s\t\t", entries[i].name);
 
         printf("\n");
